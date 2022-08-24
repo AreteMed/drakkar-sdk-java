102eca4267bc8ab09aaff8372a563c1cf0bc3c89
OlegAretemed <109290275+OlegAretemed@users.noreply.github.com>
2022-08-24 15:14:04 +0300

    DRAK-36 Introducing Encounters API calls

--- src/main/kotlin/io/aretemed/drakkar/client/DrakkarWebClient.kt
+++ src/main/kotlin/io/aretemed/drakkar/client/DrakkarWebClient.kt
@@ -2,6 +2,8 @@ package io.aretemed.drakkar.client
 
 import com.example.webclientconsumerkotlinsample.model.*
 import io.aretemed.drakkar.config.DrakkarWebClientProperties
+import io.aretemed.drakkar.model.Encounter
+import io.aretemed.drakkar.model.Encounters
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
@@ -34,157 +36,221 @@ class DrakkarWebClient {
             .build()
     }
 
-    private fun cleanReadOnlyRoomFields(room: Room): Room {
-        return Room(
-            notBeforeDateTime = room.notBeforeDateTime,
-            expirationDateTime = room.expirationDateTime,
-            enableChat = room.enableChat,
-            enableKnocking = room.enableKnocking,
-            enablePeopleUI = room.enablePeopleUI,
-            enablePrejoinUI = room.enablePrejoinUI,
-            enableScreenshare = room.enableScreenshare,
-            meetingJoinHook = room.meetingJoinHook
-        )
-    }
+    fun roomAPI() = RoomAPI()
+    inner class RoomAPI {
 
-    fun rooms(limit: Long? = 0L, offset: Long? = 0L): Rooms {
-        val queryParams = mutableMapOf<String, Any>()
-        if (limit != null && limit > 0) {
-            queryParams["limit"] = limit
-        }
-        if (offset != null && offset > 0) {
-            queryParams["offset"] = offset
-        }
-
-        var clientResponse: ClientResponse? = null
-        val responseBody = webClient()
-            .get()
-            .uri("api/rooms/", queryParams)
-            .exchangeToMono { response ->
-                clientResponse = response
-                if (response.statusCode().equals(HttpStatus.OK)) {
-                    return@exchangeToMono response.bodyToMono(Rooms::class.java)
-                } else {
-                    return@exchangeToMono response.bodyToMono(String::class.java)
-                }
+        fun rooms(limit: Int? = 0, offset: Int? = 0): Rooms {
+            val queryParams = mutableMapOf<String, Any>()
+            if (limit != null && limit > 0) {
+                queryParams["limit"] = limit
             }
-            .block()
+            if (offset != null && offset > 0) {
+                queryParams["offset"] = offset
+            }
 
-        if (responseBody is Rooms) {
-            return responseBody
-        } else {
-            throw RestClientException(
-                responseBody!!.toString(),
-                clientResponse?.createException()?.block() as Throwable
+            var clientResponse: ClientResponse? = null
+            val responseBody = webClient()
+                .get()
+                .uri("api/rooms/", queryParams)
+                .exchangeToMono { response ->
+                    clientResponse = response
+                    if (response.statusCode().equals(HttpStatus.OK)) {
+                        return@exchangeToMono response.bodyToMono(Rooms::class.java)
+                    } else {
+                        return@exchangeToMono response.bodyToMono(String::class.java)
+                    }
+                }
+                .block()
+
+            if (responseBody is Rooms) {
+                return responseBody
+            } else {
+                throw RestClientException(
+                    responseBody!!.toString(),
+                    clientResponse?.createException()?.block() as Throwable
+                )
+            }
+        }
+
+        fun room(id: String): Room {
+            var clientResponse: ClientResponse? = null
+            val responseBody = webClient()
+                .get()
+                .uri("api/rooms/${id}")
+                .exchangeToMono { response ->
+                    clientResponse = response
+                    if (response.statusCode().equals(HttpStatus.OK)) {
+                        return@exchangeToMono response.bodyToMono(Room::class.java)
+                    } else {
+                        return@exchangeToMono response.bodyToMono(String::class.java)
+                    }
+                }
+                .block()
+
+            if (responseBody is Room) {
+                return responseBody
+            } else {
+                throw RestClientException(
+                    responseBody!!.toString(),
+                    clientResponse?.createException()?.block() as Throwable
+                )
+            }
+        }
+
+        fun createRoom(room: Room): CreateRoomStatus {
+            var clientResponse: ClientResponse? = null
+            val responseBody = webClient()
+                .post()
+                .uri("api/rooms/create-room/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(room), Room::class.java)
+                .exchangeToMono { response ->
+                    clientResponse = response
+                    if (response.statusCode().equals(HttpStatus.OK)) {
+                        return@exchangeToMono response.bodyToMono(CreateRoomStatus::class.java)
+                    } else {
+                        return@exchangeToMono response.bodyToMono(String::class.java)
+                    }
+
+                }
+                .block()
+
+            if (responseBody is CreateRoomStatus) {
+                return responseBody
+            } else {
+                throw RestClientException(
+                    responseBody!!.toString(),
+                    clientResponse?.createException()?.block() as Throwable
+                )
+            }
+        }
+
+        fun updateRoom(room: Room): Room {
+            var clientResponse: ClientResponse? = null
+            val responseBody = webClient()
+                .put()
+                .uri("api/rooms/${room.id}/update-room/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(cleanReadOnlyRoomFields(room)), Room::class.java)
+                .exchangeToMono { response ->
+                    clientResponse = response
+                    if (response.statusCode().equals(HttpStatus.OK)) {
+                        return@exchangeToMono response.bodyToMono(Room::class.java)
+                    } else {
+                        return@exchangeToMono response.bodyToMono(String::class.java)
+                    }
+                }
+                .block()
+
+            if (responseBody is Room) {
+                return responseBody
+            } else {
+                throw RestClientException(
+                    responseBody!!.toString(),
+                    clientResponse?.createException()?.block() as Throwable
+                )
+            }
+        }
+
+        private fun cleanReadOnlyRoomFields(room: Room): Room {
+            return Room(
+                notBeforeDateTime = room.notBeforeDateTime,
+                expirationDateTime = room.expirationDateTime,
+                enableChat = room.enableChat,
+                enableKnocking = room.enableKnocking,
+                enablePeopleUI = room.enablePeopleUI,
+                enablePrejoinUI = room.enablePrejoinUI,
+                enableScreenshare = room.enableScreenshare,
+                meetingJoinHook = room.meetingJoinHook
             )
         }
-    }
 
-    fun room(id: String): Room {
-        var clientResponse: ClientResponse? = null
-        val responseBody = webClient()
-            .get()
-            .uri("api/rooms/${id}")
-            .exchangeToMono { response ->
-                clientResponse = response
-                if (response.statusCode().equals(HttpStatus.OK)) {
-                    return@exchangeToMono response.bodyToMono(Room::class.java)
-                } else {
-                    return@exchangeToMono response.bodyToMono(String::class.java)
+        fun createMeetingToken(createMeetingTokenInfo: CreateMeetingTokenInfo): CreateMeetingTokenStatus {
+            var clientResponse: ClientResponse? = null
+            val responseBody = webClient()
+                .post()
+                .uri("api/rooms/${createMeetingTokenInfo.roomId}/create-meeting-token/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(createMeetingTokenInfo), CreateMeetingTokenInfo::class.java)
+                .exchangeToMono { response ->
+                    clientResponse = response
+                    if (response.statusCode().equals(HttpStatus.OK)) {
+                        return@exchangeToMono response.bodyToMono(CreateMeetingTokenStatus::class.java)
+                    } else {
+                        return@exchangeToMono response.bodyToMono(String::class.java)
+                    }
+
                 }
-            }
-            .block()
+                .block()
 
-        if (responseBody is Room) {
-            return responseBody
-        } else {
-            throw RestClientException(
-                responseBody!!.toString(),
-                clientResponse?.createException()?.block() as Throwable
-            )
+            if (responseBody is CreateMeetingTokenStatus) {
+                return responseBody
+            } else {
+                throw RestClientException(
+                    responseBody!!.toString(),
+                    clientResponse?.createException()?.block() as Throwable
+                )
+            }
         }
     }
 
-    fun createRoom(room: Room): CreateRoomStatus {
-        var clientResponse: ClientResponse? = null
-        val responseBody = webClient()
-            .post()
-            .uri("api/rooms/create-room/")
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(Mono.just(room), Room::class.java)
-            .exchangeToMono { response ->
-                clientResponse = response
-                if (response.statusCode().equals(HttpStatus.OK)) {
-                    return@exchangeToMono response.bodyToMono(CreateRoomStatus::class.java)
-                } else {
-                    return@exchangeToMono response.bodyToMono(String::class.java)
-                }
-
+    fun encounterAPI() = EncounterAPI()
+    inner class EncounterAPI {
+        fun encounters(limit: Int? = 0, offset: Int? = 0): Encounters {
+            val queryParams = mutableMapOf<String, Any>()
+            if (limit != null && limit > 0) {
+                queryParams["limit"] = limit
             }
-            .block()
+            if (offset != null && offset > 0) {
+                queryParams["offset"] = offset
+            }
 
-        if (responseBody is CreateRoomStatus) {
-            return responseBody
-        } else {
-            throw RestClientException(
-                responseBody!!.toString(),
-                clientResponse?.createException()?.block() as Throwable
-            )
+            var clientResponse: ClientResponse? = null
+            val responseBody = webClient()
+                .get()
+                .uri("api/encounters/", queryParams)
+                .exchangeToMono { response ->
+                    clientResponse = response
+                    if (response.statusCode().equals(HttpStatus.OK)) {
+                        return@exchangeToMono response.bodyToMono(Encounters::class.java)
+                    } else {
+                        return@exchangeToMono response.bodyToMono(String::class.java)
+                    }
+                }
+                .block()
+
+            if (responseBody is Encounters) {
+                return responseBody
+            } else {
+                throw RestClientException(
+                    responseBody!!.toString(),
+                    clientResponse?.createException()?.block() as Throwable
+                )
+            }
         }
-    }
-
-    fun updateRoom(room: Room): Room {
-        var clientResponse: ClientResponse? = null
-        val responseBody = webClient()
-            .put()
-            .uri("api/rooms/${room.id}/update-room/")
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(Mono.just(cleanReadOnlyRoomFields(room)), Room::class.java)
-            .exchangeToMono { response ->
-                clientResponse = response
-                if (response.statusCode().equals(HttpStatus.OK)) {
-                    return@exchangeToMono response.bodyToMono(Room::class.java)
-                } else {
-                    return@exchangeToMono response.bodyToMono(String::class.java)
+        fun encounter(id: String): Encounter {
+            var clientResponse: ClientResponse? = null
+            val responseBody = webClient()
+                .get()
+                .uri("api/encounters/${id}")
+                .exchangeToMono { response ->
+                    clientResponse = response
+                    if (response.statusCode().equals(HttpStatus.OK)) {
+                        return@exchangeToMono response.bodyToMono(Encounter::class.java)
+                    } else {
+                        return@exchangeToMono response.bodyToMono(String::class.java)
+                    }
                 }
+                .block()
+
+            if (responseBody is Encounter) {
+                return responseBody
+            } else {
+                throw RestClientException(
+                    responseBody!!.toString(),
+                    clientResponse?.createException()?.block() as Throwable
+                )
             }
-            .block()
-
-        if (responseBody is Room) {
-            return responseBody
-        } else {
-            throw RestClientException(
-                responseBody!!.toString(),
-                clientResponse?.createException()?.block() as Throwable
-            )
-        }
-    }
-
-    fun createMeetingToken(createMeetingTokenInfo: CreateMeetingTokenInfo): CreateMeetingTokenStatus {
-        var clientResponse: ClientResponse? = null
-        val responseBody = webClient()
-            .post()
-            .uri("api/rooms/${createMeetingTokenInfo.roomId}/create-meeting-token/")
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(Mono.just(createMeetingTokenInfo), CreateMeetingTokenInfo::class.java)
-            .exchangeToMono { response ->
-                clientResponse = response
-                if (response.statusCode().equals(HttpStatus.OK)) {
-                    return@exchangeToMono response.bodyToMono(CreateMeetingTokenStatus::class.java)
-                } else {
-                    return@exchangeToMono response.bodyToMono(String::class.java)
-                }
-
-            }
-            .block()
-
-        if (responseBody is CreateMeetingTokenStatus) {
-            return responseBody
-        } else {
-            throw RestClientException(
-                responseBody!!.toString(),
-                clientResponse?.createException()?.block() as Throwable
-            )
         }
     }
 
--- src/main/kotlin/io/aretemed/drakkar/model/ErrorStatus.kt
+++
@@ -1,7 +0,0 @@
-package io.aretemed.drakkar.model
-
-data class ErrorStatus(
-    val success: Boolean,
-    val error: String,
-    val info: String
-)
