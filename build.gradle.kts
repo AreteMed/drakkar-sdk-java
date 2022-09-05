19dcc68856bc85df6a8ba04d54604bd17affc406
OlegAretemed <109290275+OlegAretemed@users.noreply.github.com>
2022-09-05 21:18:28 +0300

    DRAK-73 Test Coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     id ("org.jetbrains.dokka") version "1.7.10"
     `maven-publish`
     signing
+    jacoco
 }
 
 group = "io.aretemed.drakkar"
@@ -109,4 +110,19 @@ tasks.withType<Test> {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.withType(JacocoReport::class.java).all {
+    reports {
+        xml.required.set(true)
+        xml.outputLocation.set(File("$buildDir/reports/jacoco/report.xml"))
+    }
+}
+
+tasks.withType<Test> {
+    jacoco {
+        toolVersion = "0.8.3"
+        reportsDirectory.set(File("$buildDir/reports/jacoco"))
+    }
+    finalizedBy("jacocoTestReport")
 }
