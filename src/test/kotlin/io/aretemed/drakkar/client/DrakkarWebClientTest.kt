1a2ea4dfa4d8f2949fd85c1e9f7940c6bd72ec51
OlegAretemed <109290275+OlegAretemed@users.noreply.github.com>
2022-09-05 21:08:00 +0300

    DRAK-73 Unit Tests

--- src/test/kotlin/io/aretemed/drakkar/client/DrakkarWebClientTest.kt
+++ src/test/kotlin/io/aretemed/drakkar/client/DrakkarWebClientTest.kt
@@ -1,8 +1,8 @@
 package io.aretemed.drakkar.client
 
-import io.aretemed.drakkar.model.Rooms
-import com.google.gson.Gson
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.aretemed.drakkar.config.DrakkarWebClientProperties
+import io.aretemed.drakkar.model.*
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.AfterAll
@@ -14,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import java.io.IOException
+import java.util.*
 import kotlin.test.assertEquals
 
 
@@ -23,6 +24,7 @@ class DrakkarWebClientTest {
 
     var mockServer: MockWebServer = MockWebServer()
     var webClient = DrakkarWebClient()
+    val jsonMapper = jacksonObjectMapper()
 
     @BeforeAll
     @Throws(IOException::class)
@@ -46,15 +48,141 @@ class DrakkarWebClientTest {
     @Test
     fun rooms() {
         val roomsMock = Rooms(count = 50, next = "next", previous = "previous", results = emptyList())
+
         mockServer.enqueue(
             MockResponse()
-                .setBody(Gson().toJson(roomsMock))
+                .setBody(jsonMapper.writeValueAsString(roomsMock))
                 .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
         )
+
         val roomsFromResponse = webClient.roomAPI().rooms()
         val recordedRequest = mockServer.takeRequest()
         assertEquals("GET", recordedRequest.method)
         assertEquals("/api/rooms/", recordedRequest.path)
-        assertEquals(roomsMock, roomsFromResponse)
+        assertEquals(jsonMapper.writeValueAsString(roomsMock), jsonMapper.writeValueAsString(roomsFromResponse))
     }
+
+    @Test
+    fun room() {
+        val roomMock = Room(
+            id = "12345",
+            name = "TestRoom",
+            business = "55555",
+            notBeforeDateTime = Date(),
+            expirationDateTime = Date()
+        )
+
+        mockServer.enqueue(
+            MockResponse()
+                .setBody(jsonMapper.writeValueAsString(roomMock))
+                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        )
+
+        val roomFromResponse = webClient.roomAPI().room(roomMock.id!!)
+        val recordedRequest = mockServer.takeRequest()
+        assertEquals("GET", recordedRequest.method)
+        assertEquals("/api/rooms/${roomMock.id}/", recordedRequest.path)
+        assertEquals(jsonMapper.writeValueAsString(roomMock), jsonMapper.writeValueAsString(roomFromResponse))
+    }
+
+    @Test
+    fun createRoom() {
+        val createRoomStatusMock =
+            CreateRoomStatus(success = true, url = "https://localhost/embedded/ABCDEFG1234567/", id = "abcde12345")
+
+        mockServer.enqueue(
+            MockResponse()
+                .setBody(jsonMapper.writeValueAsString(createRoomStatusMock))
+                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        )
+
+        val createRoomStatusFromResponse = webClient.roomAPI().createRoom(Room())
+        val recordedRequest = mockServer.takeRequest()
+        assertEquals("POST", recordedRequest.method)
+        assertEquals("/api/rooms/create-room/", recordedRequest.path)
+        assertEquals(jsonMapper.writeValueAsString(createRoomStatusMock), jsonMapper.writeValueAsString(createRoomStatusFromResponse))
+    }
+
+    @Test
+    fun updateRoom() {
+
+        val roomMock = Room(
+            id = "12345",
+            name = "TestRoom",
+            business = "55555",
+            notBeforeDateTime = Date(),
+            expirationDateTime = Date()
+        )
+
+        mockServer.enqueue(
+            MockResponse()
+                .setBody(jsonMapper.writeValueAsString(roomMock))
+                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        )
+
+        val roomFromResponse = webClient.roomAPI().updateRoom(roomMock)
+        val recordedRequest = mockServer.takeRequest()
+        assertEquals("PUT", recordedRequest.method)
+        assertEquals("/api/rooms/${roomMock.id}/update-room/", recordedRequest.path)
+        assertEquals(jsonMapper.writeValueAsString(roomMock), jsonMapper.writeValueAsString(roomFromResponse))
+    }
+
+    @Test
+    fun createMeetingToken() {
+        val createMeetingTokenInfo = CreateMeetingTokenInfo(roomId = "12345", userId = "56789", userName = "TestUser", isOwner = true)
+        val createMeetingTokenStatusMock = CreateMeetingTokenStatus(success = true, url = "https://localhost/ABCDEFG1234567?t=abcde12345")
+
+        mockServer.enqueue(
+            MockResponse()
+                .setBody(jsonMapper.writeValueAsString(createMeetingTokenStatusMock))
+                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        )
+
+        val createMeetingTokenStatusFromResponse = webClient.roomAPI().createMeetingToken(createMeetingTokenInfo)
+        val recordedRequest = mockServer.takeRequest()
+        assertEquals("POST", recordedRequest.method)
+        assertEquals("/api/rooms/${createMeetingTokenInfo.roomId}/create-meeting-token/", recordedRequest.path)
+        assertEquals(jsonMapper.writeValueAsString(createMeetingTokenStatusMock), jsonMapper.writeValueAsString(createMeetingTokenStatusFromResponse))
+    }
+
+    @Test
+    fun encounters() {
+        val encountersMock = Encounters(count = 50, next = "next", previous = "previous", results = emptyList())
+
+        mockServer.enqueue(
+            MockResponse()
+                .setBody(jsonMapper.writeValueAsString(encountersMock))
+                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        )
+
+        val encountersFromResponse = webClient.encounterAPI().encounters()
+        val recordedRequest = mockServer.takeRequest()
+        assertEquals("GET", recordedRequest.method)
+        assertEquals("/api/encounters/", recordedRequest.path)
+        assertEquals(jsonMapper.writeValueAsString(encountersMock), jsonMapper.writeValueAsString(encountersFromResponse))
+    }
+
+    @Test
+    fun encounter() {
+        val encounterMock = Encounter(
+            id = "12345",
+            room = "1234567",
+            scheduledStartDateTime = Date(),
+            created = Date(),
+            updated = Date()
+        )
+
+        mockServer.enqueue(
+            MockResponse()
+                .setBody(jsonMapper.writeValueAsString(encounterMock))
+                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        )
+
+        val encounterFromResponse = webClient.encounterAPI().encounter(encounterMock.id!!)
+        val recordedRequest = mockServer.takeRequest()
+        assertEquals("GET", recordedRequest.method)
+        assertEquals("/api/encounters/${encounterMock.id}/", recordedRequest.path)
+        assertEquals(jsonMapper.writeValueAsString(encounterMock), jsonMapper.writeValueAsString(encounterFromResponse))
+    }
+
 }
